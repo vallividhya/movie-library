757b320c72468f0a1516a668d4ed9454d024f16b
oscargrp <oscargrp@gmail.com>
2012-11-07 17:19:26 -0800

    Admin Bean

---
+++ src/edu/sjsu/videolibrary/model/Admin.java
@@ -0,0 +1,35 @@
+package edu.sjsu.videolibrary.model;
+
+public class Admin {
+	private String adminId; 
+	private String password;
+	private String firstName;
+	private String lastName; 
+	
+	public String getAdminId() {
+		return adminId;
+	}
+	public void setAdminId(String adminId) {
+		this.adminId = adminId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	} 
+	
+	
+}
