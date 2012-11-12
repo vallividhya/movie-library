28168a06a618b9372af5bcfe6e08d3373e749ccc
Rohini <rohini.thakar85@gmail.com>
2012-11-11 17:13:36 -0800

    Changed Utils

--- src/edu/sjsu/videolibrary/util/Utils.java
+++ src/edu/sjsu/videolibrary/util/Utils.java
@@ -19,15 +19,7 @@ import org.apache.commons.validator.routines.EmailValidator;
 
 public class Utils {
 
-	static public boolean validateLogin( HttpServletRequest request, HttpServletResponse response ) throws IOException {
-		String user =(String) request.getSession().getAttribute("user");
-		if( user == null ) {
-			response.sendRedirect("SignIn");
-			return false;
-		}
-		return true;
-	}
-
+	
 	static public String encryptPassword(String pwd){
 		String securePwd = null;
 		byte[] bytesOfMessage;
@@ -54,33 +46,6 @@ public class Utils {
 		return in != null && !in.isEmpty() && !in.trim().isEmpty();
 	}
 
-	static public boolean isValidCardNum(String ccNum){
-		if(isValidInput(ccNum) && ccNum.length()==16){
-			for(char temp :ccNum.toCharArray()){
-				if(temp < '0'|| temp>'9'){
-					return false;
-				}
-			}
-			return true;
-		}
-		else{
-			return false;
-		}
-	}
-
-	static public void generateHeader( HttpServletRequest request, HttpServletResponse response ) throws IOException {
-		HttpSession session = request.getSession();
-		PrintWriter out = response.getWriter();
-		out.write("<table border=1><tr>");
-		out.write("<td><a href=\"UserMain\">View Products</a></td>");
-		out.write("<td><a href=\"ViewAccount\">View My Account</a></td>");
-		out.write("	<td><a href=\"Cart\">Shopping Cart</a></td>");
-		out.write("	<td><a href=\"SellItem\">Sell an item</a></td>");
-		out.write("	<td>Last Logged In:"+((Date)session.getAttribute("lastLoggedInTime")).toString()+"</td>");
-		out.write("	<td><a href=\"SignOutServlet\">Sign-off</a></td>");
-		out.write("</tr>");
-		out.write("</table>");
-	}
 
 	static public boolean isValidEMailAddress(String emailStr){
 		if(isValidInput(emailStr)){
