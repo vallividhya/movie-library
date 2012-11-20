03fdc159d185e5f901e754fc719f0d5b4f5e58f8
Rohini <rohini.thakar85@gmail.com>
2012-11-19 16:27:17 -0800

    test push

--- src/edu/sjsu/videolibrary/db/AdminDAO.java
+++ src/edu/sjsu/videolibrary/db/AdminDAO.java
@@ -4,7 +4,10 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
@@ -538,6 +541,6 @@ public class AdminDAO extends VideoLibraryDAO {
 		User[] array= list.toArray(new User[list.size()]);
 		return array;
 	}
-
-
 }
+	
+	
--- src/edu/sjsu/videolibrary/exception/NoCategoryFoundException.java
+++ src/edu/sjsu/videolibrary/exception/NoCategoryFoundException.java
@@ -18,7 +18,7 @@ public class NoCategoryFoundException extends Exception{
 	 */
 	public NoCategoryFoundException(String arg0, Throwable arg1, boolean arg2,
 			boolean arg3) {
-		super(arg0, arg1, arg2, arg3);
+//		super(arg0, arg1, arg2, arg3);
 		// TODO Auto-generated constructor stub
 	}
 
