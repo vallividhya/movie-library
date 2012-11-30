a3aaf64189bd3d8155699790ae2599553218ed41
Rohini <rohini.thakar85@gmail.com>
2012-11-30 15:59:52 -0800

    added a package and class file for JMS function

---
+++ src/edu/sjsu/videolibrary/jms/Videolibrary_JMSServer.java
@@ -0,0 +1,14 @@
+//package edu.sjsu.videolibrary.jms;
+//
+////import javax.jms.*;
+//
+//
+////public class Videolibrary_JMSServer implements MessageListener {
+//	
+//	public static void main(String args[])
+//	{
+//		new Videolibrary_JMSServer();
+//	}
+//	
+//
+//}
