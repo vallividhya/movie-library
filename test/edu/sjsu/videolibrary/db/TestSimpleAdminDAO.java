0c68b2733b96af3f3a1c9e2b92c25c8d0fd41830
Neha <Neha@Neha-HP>
2012-12-01 12:33:32 -0800

    Added Junit test case for SimpleAdminDAO

---
+++ test/edu/sjsu/videolibrary/db/TestSimpleAdminDAO.java
@@ -0,0 +1,102 @@
+package edu.sjsu.videolibrary.db;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class TestSimpleAdminDAO extends TestCase {
+
+	@Before
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	@After
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	@Test
+	public final void testDisplayUserInformation() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testDisplayMovieInformation() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetRentAmountforMovie() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetMonthlyFeesForPremiumMember() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testUpdateMovieInfo() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGenerateMonthlyStatement() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testDeleteUser() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGenerateMonthlyBillForPremiumMember() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testDeleteAdmin() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testListMembers() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testListAdmins() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testSearchUserStringStringStringStringStringStringStringStringStringStringIntInt() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testDisplayAdminInformation() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testUpdateAdminInfo() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testUpdateUserPassword() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testSignInAdminObject() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
